93d50bf724f2e92ea4cbce180e6c08452e6887e0
Alexander Fedorov <44639231+afedorov1971@users.noreply.github.com>
2021-04-08 21:03:24 +0300

    Add project files.

---
+++ TestAws/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestAws
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			try
+			{
+				using (var client = new HttpClient())
+				{
+					var res = await client.GetAsync("http://169.254.169.254/latest/dynamic/instance-identity/document");
+
+					if (res.IsSuccessStatusCode)
+					{
+						Console.WriteLine("AWS environment");
+						var txt = await res.Content.ReadAsStringAsync();
+						Console.WriteLine($"The response is {txt}");
+					}
+					else
+					{
+						Console.WriteLine("Not AWS");
+					}
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Not AWS");
+			}
+		}
+	}
+}
---
+++ TestAws/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestAws")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("TestAws")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09b22e54-17fd-44fb-92bc-fef661d3b2f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
